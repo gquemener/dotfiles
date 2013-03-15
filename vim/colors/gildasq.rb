4f46cb6738742f8d1e294464b8936649fa1b56dc
Gildas Quéméner <gildas.quemener@knplabs.com>
2013-03-15 14:59:02 +0100

    make vim works

--- vim/colors/gildasq.rb
+++ vim/colors/gildasq.rb
@@ -1,31 +1,45 @@
 vim_colors "gildasq" do
     reset      true
     background :dark
+    author     "@GildasQ"
 
     Normal             "ffffff", "242424", :gui => "none"
     LineNr             "666666", "242424", :gui => "none"
     CursorLineNr       "cccccc", "242424", :gui => "bold"
-    Delimiter          "999999", "none", :gui => "none"
+    Delimiter          "999999", "none",   :gui => "none"
+    CursorLine         "none",   "333333", :gui => "none"
+    MatchParen         "ffffff", "none",   :gui => "bold"
+    NonText            "242424", "none",   :gui => "none"
+    netrwDir           "8ac6f2", "none",   :gui => "none"
+    netrwClassify      "8ac6f2", "none",   :gui => "none"
+    netrwComment       "8ac6f2", "none",   :gui => "none"
+
+    #LustyJuggler
+    LustySelected      "ffaa00", "none",   :gui => "none"
+    LustyDir           "8ac6f2", "none",   :gui => "none"
 
     # PHP
     phpStructure       "ffaa00", "none",   :gui => "none" # class, extends, implements, ...
+    phpFunctions       "ffaa00", "none",   :gui => "none" # str_reaplce, ...
     phpClasses         "aaaaaa", "none",   :gui => "none" # like \Exception
     phpSpecialFunction "8ac6f2", "none",   :gui => "none"
     phpInclude         "e5786d", "none",   :gui => "none" # use, require, ...
     phpStorageClass    "cae682", "none",   :gui => "none" # public, private, protected, static, ...
     phpIdentifier      "cae682", "none",   :gui => "none" # $this
+    phpConstant        "cae682", "none",   :gui => "none" # $this
     phpMethodsVar      "ffffff", "none",   :gui => "none" # $this->foo
     phpDefine          "e5786d", "none",   :gui => "none" # function, new
-    phpParent "cccccc", "none", :gui => "none"
+    phpParent          "cccccc", "none",   :gui => "none"
 
     phpMemberSelector  "cae682", "none",   :gui => "none" # ->
     phpVarSelector     "8ac6f2", "none",   :gui => "none" # $
     phpOperator        "8ac6f2", "none",   :gui => "none" # =
+    phpRelation        "8ac6f2", "none",   :gui => "none" # >
     phpComparison      "8ac6f2", "none",   :gui => "none" # ===
     phpException       "8ac6f2", "none",   :gui => "none" # throw
     phpStatement       "8ac6f2", "none",   :gui => "none" # return
     phpConditional     "8ac6f2", "none",   :gui => "none"
-                                           
+
     phpType            "ffaa00", "none",   :gui => "none" # null, array, ...
     phpComment         "999999", "none",   :gui => "none"
 
