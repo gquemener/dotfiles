3cffa4962e9959c5bf6db5e63244f05c12b3c56f
Gildas Quéméner <gildas.quemener@gmail.com>
2022-02-17 21:59:57 +0100

    Displays relative path in lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,3 +1,11 @@
 require('lualine').setup {
-    options = { theme = 'powerline' }
+    options = { theme = 'powerline' },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            }
+        }
+    }
 }
