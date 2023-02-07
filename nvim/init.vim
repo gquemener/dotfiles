f7605da84918a0dbf4fb59547327bc3bca949878
Gildas Quéméner <gildas.quemener@codurance.com>
2023-02-07 16:35:43 +0100

    Drastically simplify nvim configuration
    Based on https://github.com/nvim-lua/kickstart.nvim

--- nvim/init.vim
+++
@@ -1,4 +0,0 @@
-source ~/.config/nvim/general.vim
-source ~/.config/nvim/plugins.vim
-source ~/.config/nvim/visual.vim
-source ~/.config/nvim/bindings.vim
--- nvim/plugin/fuzzy-finder.lua
+++
@@ -1,7 +0,0 @@
-require('telescope').setup()
-
-vim.api.nvim_set_keymap('n', '<c-p>', ':Telescope find_files<cr>', { noremap = true })
---vim.api.nvim_set_keymap('n', '<c-m>', ':Telescope find_files no_ignore=true<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-f>', ':Telescope live_grep<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope grep_string<cr>', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>f', '"zy:Telescope live_grep default_text=<C-r>z<cr>', { noremap = true })
