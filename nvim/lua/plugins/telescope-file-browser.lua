5e4930ef7ac00660ccf28244cfe96d615f2652e3
Gildas Quéméner <gildas.quemener@dashdoc.com>
2026-09-22 11:33:52 +0200

    Toggle hidden files while browsing

--- nvim/lua/plugins/telescope-file-browser.lua
+++ nvim/lua/plugins/telescope-file-browser.lua
@@ -1,19 +1,35 @@
 return {
-  "nvim-telescope/telescope-file-browser.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  config = function()
-    require("telescope").load_extension("file_browser")
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>lf",
-      ":Telescope file_browser display_stat=false grouped=true<CR>",
-      { noremap = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>lr",
-      ":Telescope file_browser path=%:p:h select_buffer=true display_stat=false grouped=true<CR>",
-      { noremap = true }
-    )
-  end,
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+        local fb_actions = require("telescope._extensions.file_browser.actions")
+
+        require("telescope").setup({
+            extensions = {
+                file_browser = {
+                    mappings = {
+                        ["n"] = {
+                            ["<C-a>"] = fb_actions.toggle_hidden,
+                        },
+                        ["i"] = {
+                            ["<C-a>"] = fb_actions.toggle_hidden,
+                        },
+                    },
+                },
+            },
+        })
+        require("telescope").load_extension("file_browser")
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>lf",
+            ":Telescope file_browser display_stat=false grouped=true<CR>",
+            { noremap = true }
+        )
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>lr",
+            ":Telescope file_browser path=%:p:h select_buffer=true display_stat=false grouped=true<CR>",
+            { noremap = true }
+        )
+    end,
 }
